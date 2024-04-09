78247c0e61988cda43a94ac8e601248a7056dd48
Ssali Joshua <ssalijoshua2002@gmail.com>
2024-04-09 19:26:44 +0300

    First 30 minutes completed

---
+++ script.js
@@ -0,0 +1,26 @@
+// printing to the console
+console.log("Hello world");
+
+
+// Variable that you can update later
+var myName = 'Hindshu Pichai';
+
+// Variable that you can only use within the scope that you have created it
+let home = 'Mpororo';
+
+// Variable that is a constant and will never change 
+const pi = 3.1428;
+
+// Javascript is a case sensitive language
+var cow = 25;
+
+// Escape character in Javascript "\"
+var Sentence = "I am \"the best \"man in 'India"  //Another way is to ise single quotes
+
+// Concatenating using +=
+var x = "She is thick "
+
+x += "She is thin"
+
+// Counting the number of characters in a variable using .length
+var x = "You are the best boy in the world" //result is 33 characters
